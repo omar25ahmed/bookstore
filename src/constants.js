35f868bccc11d470e56936b0677c77d666088c1c
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-30 11:13:23 +0200

    Implementing the constants with duck pattern

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const ADD_BOOK = 'ADD_BOOK';
-export const DELETE_BOOK = 'DELETE_BOOK';
-export const STATUS_CATEGORIES = 'STATUS_CATEGORIES';
+export const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+export const DELETE_BOOK = 'bookstore/books/REMOVE_BOOK';
+export const STATUS_CATEGORIES = 'bookstore/categories/CHECK_STATUS';
