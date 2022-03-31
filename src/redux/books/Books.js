923bf5cb810e7aa20d2068e4dec2ab282e70ef70
Omar Ragheb <ahmed.raph29@gmail.com>
2022-03-31 23:59:47 +0200

    Adding API methods

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,5 +1,7 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const GET_BOOK = 'bookstore/books/GET_BOOK';
+const api = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/dHSbS01NUEipnLIkAry3/books';
 
 const initialState = [];
 
@@ -9,19 +11,62 @@ const reducer = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case GET_BOOK: {
+      const books = [];
+      Object.entries(action.payload).forEach(
+        ([key, value]) => books.push({
+          id: key,
+          title: value[0].title,
+          author: value[0].author,
+          genre: value[0].category,
+        }),
+      );
+      return books;
+    }
     default:
       return state;
   }
 };
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  id,
-});
+// export const removeBook = (id) => ({
+//   type: REMOVE_BOOK,
+//   id,
+// });
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
+// export const addBook = (payload) => ({
+//   type: ADD_BOOK,
+//   payload,
+// });
+
+export const addBook = (payload) => async (dispatch) => {
+  await fetch(api, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: payload.id,
+      title: payload.title,
+      author: payload.author,
+      category: payload.genre,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  dispatch({ type: ADD_BOOK, payload });
+};
+
+export const removeBook = (id) => async (dispatch) => {
+  const response = await fetch(`${api}/${id}`, {
+    method: 'DELETE',
+  });
+  if (response.status === 201) {
+    dispatch({ type: REMOVE_BOOK, id });
+  }
+};
+
+export const getBooks = () => async (dispatch) => {
+  const response = await fetch(api, {
+    method: 'GET',
+  });
+  const data = await response.json();
+  dispatch({ type: GET_BOOK, payload: data });
+};
 
 export default reducer;
